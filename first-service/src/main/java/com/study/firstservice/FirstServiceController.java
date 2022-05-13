abdcea0d32bb4682cf0b6b5e3121ba50c6274207
yumichoi0921 <yumichoi0921@gmail.com>
2022-05-13 17:08:55 +0900

    feat: msa 강의

---
+++ first-service/src/main/java/com/study/firstservice/FirstServiceController.java
@@ -0,0 +1,27 @@
+package com.study.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service")
+@Slf4j
+public class FirstServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service";
+    }
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in first Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi there. This is message from First Service";
+    }
+}
